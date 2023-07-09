f82ab59ee58b148271ead98976ddc97ead24cb4e
Gaetan Fox <me@gaetanfox.io>
2023-07-09 15:04:26 +0300

    invaders movement

--- Assets/Scripts/Invaders.cs
+++ Assets/Scripts/Invaders.cs
@@ -8,6 +8,9 @@ public class Invaders : MonoBehaviour
   public int rows = 5;
   public int columns = 11;
 
+  public float speed = 10.0f;
+  public Vector3 _direction = Vector2.right;
+
   private void Awake()
   {
     for (int row = 0; row < this.rows; row++)
@@ -24,5 +27,36 @@ public class Invaders : MonoBehaviour
         invader.transform.localPosition = position;
       }
     }
+  }
+  private void Update()
+  {
+    this.transform.position += _direction * speed * Time.deltaTime;
+
+    Vector3 leftEdge = Camera.main.ViewportToWorldPoint(Vector3.zero);
+    Vector3 rightEdge = Camera.main.ViewportToWorldPoint(Vector3.right);
+
+    foreach (Transform invader in this.transform)
+    {
+      if (!invader.gameObject.activeInHierarchy)
+      {
+        continue;
+      }
+      if (_direction == Vector3.right && invader.position.x >= (rightEdge.x - 1.0f))
+      {
+        AdvanceRow();
+      }
+      else if (_direction == Vector3.left && invader.position.x <= (leftEdge.x + 1.0f))
+      {
+        AdvanceRow();
+      }
+    }
+  }
+
+  private void AdvanceRow()
+  {
+    _direction.x *= -1.0f;
+    Vector3 position = this.transform.position;
+    position.y -= 1.0f;
+    this.transform.position = position;
   }
 }
